91e7794fb7aac09ea7ecd6c6aee75a2df1920660
Kashish <kashish4sh@gmail.com>
2024-12-16 14:38:41 +0530

    Landing page completed

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,40 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
     return(
         <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            Footer
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="/HR-Croatia(Hrvatska).svg" alt="Croatian" height={32} width={40} className="mr-4 rounded-md"/>
+                    Croatian
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="/IN-India.svg" alt="India" height={32} width={40} className="mr-4 rounded-md"/>
+                    Indian
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="/ES-Spain.svg" alt="Spain" height={32} width={40} className="mr-4 rounded-md"/>
+                    Spanish
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="/DE-Germany.svg" alt="Germany" height={32} width={40} className="mr-4 rounded-md"/>
+                    German
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="/FR-France.svg" alt="France" height={32} width={40} className="mr-4 rounded-md"/>
+                    French
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="/IT-Italy.svg" alt="Italy" height={32} width={40} className="mr-4 rounded-md"/>
+                    Italian
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="JP-Japan.svg" alt="Japan" height={32} width={40} className="mr-4 rounded-md"/>
+                    Japanese
+                </Button>
+            </div>
+            
         </footer>
     );
 };
--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -33,7 +33,6 @@ export const Header = () => {
                     </SignedOut>
                 </ClerkLoaded>
             </div>
-            Header
         </header>
 
     );
