61ef9c0a517092f1ec45222833257d2f35cf6fe9
Kashish <kashish4sh@gmail.com>
2025-03-31 09:15:07 +0530

    learn page wrapper created

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,23 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { USerProgress } from "@/components/user-progress";
+import { title } from "process";
+
 const LearnPage = () => {
     return ( 
-        <div>
-            Learn Page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <USerProgress
+                    activeCourse={{ title: "Spanish", imageSrc: "/ES-Spain.svg"}}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                 />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Spanish"/>
+            </FeedWrapper>
         </div>
     );
 }
