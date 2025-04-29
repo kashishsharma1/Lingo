0004b152e95f26a485d69f6a44bbdafc5f938afb
Kashish <kashish4sh@gmail.com>
2025-04-29 17:37:08 +0530

    User Progress completed

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,10 +1,25 @@
+import { getUserProgress } from "@/db/queries";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { USerProgress } from "@/components/user-progress";
 import { title } from "process";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+
+    const userProgressData = getUserProgress();
+
+    const [
+        userProgress
+    ] = await Promise.all([
+        userProgressData
+    ]);
+
+    if(!userProgress || !userProgress.activeCourse){
+        redirect("/courses");
+    }
+
     return ( 
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
