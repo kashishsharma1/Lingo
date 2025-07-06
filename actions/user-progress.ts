a943400e70b3955b950bcb437139d38cd731793b
Kashish <kashish4sh@gmail.com>
2025-07-06 19:33:35 +0530

    Shop implemented

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -8,6 +8,8 @@ import { challengeProgress, challenges, userProgress } from "@/db/schema";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { and, eq } from "drizzle-orm";
 
+const POINTS_TO_REFILL = 10;
+
 export const upsertUserProgress = async (courseId: number) => {
     const { userId } = await auth();
     const user = await currentUser();
@@ -108,4 +110,28 @@ export const reduceHearts = async (challengeId: number) => {
     revalidatePath("/leaderboard");
     revalidatePath(`/lesson/${lessonId}`);
 
+};
+
+export const refillHearts = async () =>{
+    const currentUserProgress = await getUserProgress();
+
+    if(!currentUserProgress){
+        throw new Error("User Progress not found");
+    }
+    if(currentUserProgress.hearts === 5){
+        throw new Error("Hearts are already full");
+    }
+    if(currentUserProgress.points < POINTS_TO_REFILL){
+        throw new Error("Not enough points");
+    }
+
+    await db.update(userProgress).set({
+        hearts: 5,
+        points: currentUserProgress.points - POINTS_TO_REFILL,
+    }).where(eq(userProgress.userId, currentUserProgress.userId));
+
+    revalidatePath("/shop");
+    revalidatePath("/learn");
+    revalidatePath("/quests");
+    revalidatePath("/leaderboard");
 };
